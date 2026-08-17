f190face1c9d318c2fecce4a73e0451988ee0a05
isabellyalvescorreia <isabelly.alves.correia@escola.pr.gov.br>
2026-08-17 09:53:33 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+ // Usamos querySelectorAll para capturar TODOS os botões
+        const botoes = document.querySelectorAll("button");
 
+        // Percorremos a lista de botões
+        botoes.forEach(function(botao) {
+            let curtiu = false;
+            botao.addEventListener("click", function() {
+                console.log("fui clicado");
+                let texto = botao.querySelector("span");
+                if (curtiu === false) {
+                    texto.textContent++;
+                    curtiu = true;
+                } else{
+                    texto.textContent --;
+                    curtiu = false;
+                }
+            });
+        });
